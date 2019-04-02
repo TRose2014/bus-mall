46b3cf960463b193fb68677cedb97521fbc42ed6
Tia Rose <shortyshort789@aol.com>
2019-04-01 22:56:11 -0700

    have image randomly generating at the bottom of the body

--- js/app.js
+++ js/app.js
@@ -24,22 +24,53 @@ var productSrc = [
   './img/wine-glass.jpg'
 ];
 
+var product = [];
 var totalVotes = 0;
 var totalViews = 0;
+
+function Product(imgSrcPath, name, HTMLid){
+  this.imgSrcPath = imgSrcPath;
+  this.name = name;
+  this.totalVotes = this.totalViews = 0;
+  this.HTMLid = HTMLid;
+  product[this.HTMLid] = this;
+}
+Product.prototype.calculatePercentage = function(){
+  return this.totalVotes / this.totalViews;
+};
+Product.prototype.render = function(parentId){
+  var parent = document.getElementById(parentId);
+
+  var img = document.getElementById('img');
+  img.setAttribute('id', this.HTMLid);
+  img.setAttribute('src', this.imgSrcPath);
+  img.setAttribute('class', 'product');
+
+}
+
 
 var content = document.getElementById('content');
 
 var voteForAnImage = function(event){
   if(event.target.className === 'product'){
-    console.log('Nice');
+    product[event.target.id].totalVotes++;
+    if(totalVotes === 25){
+      console.log('Done');
+    }
   }
 };
 content.addEventListener('click', voteForAnImage);
 
-var displayRandomImage = function(){
 
+//Generating Random Picture and attaching it to the body
+var displayRandomImage = function(productsArray){
+  var newImage = Math.floor(Math.random() * productsArray.length);
+  
+  //stackoverflow - https://stackoverflow.com/questions/14004318/show-random-image-from-array-in-javascript
+  var randomImage = productsArray[newImage];
+  var randomImageString = '<img src="'+ randomImage + '" alt = "test">';
+  document.write(randomImageString); document.close();
+  console.log('<img src="'+ randomImage);
 };
 
-var calculatePercentage = function(){
-  var percentage = totalVotes / totalViews;
-}
+displayRandomImage(productSrc);
