08c25984adb8ea28504201ff8049af1f755fc0d0
Tia Rose <shortyshort789@aol.com>
2019-04-03 23:24:49 -0700

    have images appearing one at a time, working on local-storage but not there yet

--- js/app.js
+++ js/app.js
@@ -15,7 +15,7 @@ var productSrc = [
   ['./img/pet-sweep.jpg', 'Pet-Sweep', 'pet-sweep'],
   ['./img/scissors.jpg', 'Scissors', 'scissors'],
   ['./img/shark.jpg', 'Shark', 'shark'],
-  ['./img/sweep.jpg', 'Sweep', 'sweep'],
+  ['./img/sweep.png', 'Sweep', 'sweep'],
   ['./img/tauntaun.jpg', 'Tauntaun', 'tauntaun'],
   ['./img/unicorn.jpg', 'Unicorn', 'unicorn'],
   ['./img/usb.gif', 'USB', 'usb'],
@@ -28,6 +28,7 @@ var PRODUCTS = {};
 var totalVotesOnPage = 0;
 // var totalViews = 0;
 var lastCycleImage = [];
+var currentCycle = [];
 
 function Product(imgSrcPath, name, HTMLid){
   this.imgSrcPath = imgSrcPath;
@@ -55,6 +56,7 @@ Product.prototype.render = function(parentId){
 //Makes List Visible after so many clicks
 var resultList = function(){
   document.getElementById('resultList').style.visibility = 'visible';
+  document.getElementById('myChart').style.visibility = 'visible';
 };
 
 
@@ -65,19 +67,18 @@ var voteForAnImage = function(event){
     // console.log('Done');
     totalVotesOnPage++;
     console.log(totalVotesOnPage);
-    setStateToLocalStorage();
-    //---------NEED TO GET TO WORK----------------//
-    // product[event.target.id].totalVotes++;
+    selectionState.totalVotesOnPage = totalVotesOnPage;
+  
+    displayRandomImage(productSrc);
     // PRODUCTS[event.target.id].totalVotes++;
-    // console.log(totalVotes);
-    // console.log(PRODUCTS[event.target.id]);
+    
     if(totalVotesOnPage === 5){
       content.removeEventListener('click', voteForAnImage);
       console.log(totalVotesOnPage + ' votes completed');
       resultList();
-      setStateToLocalStorage();
     }
   }
+  setStateToLocalStorage();
 };
 
 content.addEventListener('click', voteForAnImage);
@@ -95,21 +96,27 @@ var displayRandomImage = function(productsArray){
   console.log(lastCycleImage);
   //create empty array, push randomimage path into an array and use array.protoype find to get the index number
 
+  //have img paths being pushed into empty array one by one until it hits 5
+  //use function to shift 3 images out of array. Still need to have images appear at the same time
+  
   var imageOne = document.getElementById('imgOne');
-  imageOne.setAttribute('src', lastCycleImage);
+  imageOne.setAttribute('src', lastCycleImage[0]);
 
   var imageTwo = document.getElementById('imgTwo');
-  imageTwo.setAttribute('src', './img/boots.jpg');
+  imageTwo.setAttribute('src', lastCycleImage[1]);
 
   var imageThree = document.getElementById('imgThree');
-  imageThree.setAttribute('src', lastCycleImage);
+  imageThree.setAttribute('src', lastCycleImage[2]);
+
+  selectionState.currentCycle = currentCycle;
+  selectionState.lastCycleImage = lastCycleImage;
 
 };
 
-displayRandomImage(productSrc);
+// displayRandomImage(productSrc);
 
 // var getNextImages = function(){
-//   for(var i = 0; i < 3; i++){
+//   for(var i = 0; i < 4; i++){
 //     var randomIndex = displayRandomImage();
 //     while(lastCycleImage.includes(randomIndex)){
 //       randomIndex = displayRandomImage();
@@ -125,33 +132,71 @@ displayRandomImage(productSrc);
 //   randomProduct.render('imgOne');
 // }
 
+//---------------------------
+//
+// Chart
+//
+//---------------------------
+
+var ctx = document.getElementById('myChart').getContext('2d');
+var myChart = new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: ['Bathroom', 'Shark', 'Unicorn', 'Dragon', 'Pen'],
+    datasets: [{
+      label: '# of Votes',
+      data: [3, 4, 2, 5, 1],
+      backgroundColor: [
+        'rgb(255, 150, 132)',
+        'rgb(54, 162, 235)',
+        'rgb(255, 206, 86)',
+        'rgb(75, 192, 192)',
+        'rgb(153, 2, 255)',
+      ],
+      borderColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+      ],
+      borderWidth: 1
+
+    }]
+  },
+});
+
 //-------------
 //JSON
 //-------------
 
-var STATE_KEY = 'counterState';
-var counterState = {};
+var STATE_KEY = 'voteState';
+var selectionState = {
+  totalVotesOnPage: 0,
+  lastCycleImage: [],
+  currentCycle: [],
+};
 // 1. What to put in local storage
-// totalViews - totalVotes - totalVotesOnPage - CurrentCycle - PreviousCyle
+// totalViews - totalVotes - totalVotesOnPage - currentCycle - PreviousCyle
 
 //2. When to update local storage
 // totalViews - totalVotes - totalVotesOnPage
 
 
 function setStateToLocalStorage(){
-  localStorage.setItem(STATE_KEY, JSON.stringify(counterState));
+  localStorage.setItem(STATE_KEY, JSON.stringify(selectionState));
 }
 
 // function renderVotes(){
-//   totalVotesOnPage = counterState.totalVotesOnPage;
-//   lastCycleImage = counterState.lastCycleImage;
+//   counterState.totalVotesOnPage = totalVotesOnPage;
+//   counterState.lastCycleImage = lastCycleImage;
 // }
 
 function getStateFromLocalStorage(){
   if(localStorage[STATE_KEY]){
     var rawState = localStorage.getItem(STATE_KEY);
-    counterState = JSON.parse(rawState);
+    selectionState = JSON.parse(rawState);
   }
 }
 setStateToLocalStorage();
-renderVotes();
+// renderVotes();
